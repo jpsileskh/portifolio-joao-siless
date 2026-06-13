0a39d187d1d0e0d4d0042b657ad3fc7b9d667bb4
João Siles <jpsileskh@hotmail.com>
2026-06-12 22:01:07 -0300

    coloca texto

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Teste Portfolio</h1>
+      <p>Eu gosto muito de jogar videogayme. Jogos como todos os genêros menos futebol me fascinam</p>
     </div>
   );
 }
